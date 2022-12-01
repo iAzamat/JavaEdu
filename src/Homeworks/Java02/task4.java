f0ccf9317c0aecc75aa3eae5126738f691d01793
iAzamat <isabirovazamati@gmail.com>
2022-12-01 21:46:41 +0500

    Пересоздана структура папок

---
+++ src/Homeworks/Java02/task4.java
@@ -0,0 +1,8 @@
+package Homeworks.Java02;
+
+/*
+К калькулятору из предыдущего дз добавить логирование
+ */
+
+public class task4 {
+}
---
+++ src/Seminars/Java01/Main2.java
@@ -0,0 +1,8 @@
+package Seminars.Java01;
+
+public class Main2 {
+    // имя файла должно совпадать с именем класса (public)
+    public static void main(String[] args) throws Exception {
+
+    }
+}
